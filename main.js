02b36d638e48ffbde1c944531bf440462d3e05a6
Junaid Ahmed Shaikh <117148118+junaidahmedshaikh@users.noreply.github.com>
2023-10-25 21:02:01 +0530

    version 2.4

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 
 
 
-const allNavbarLists = document.querySelectorAll(".navbarList")
-const windowLocation = window.location.pathname;
+// const allNavbarLists = document.querySelectorAll(".navbarList")
+// const windowLocation = window.location.pathname;
 
-// console.log(currentNavbarLocation)
+// // console.log(currentNavbarLocation)
 
-allNavbarLists.forEach(allNavbarList => {
+// allNavbarLists.forEach(allNavbarList => {
 
-    const newNavbarPathname = new URL(allNavbarList.href).pathname;
+//     const newNavbarPathname = new URL(allNavbarList.href).pathname;
     
-    if(windowLocation == newNavbarPathname){
-        allNavbarList.classList.add("activeNavbarSection")
-    }
+//     if(windowLocation == newNavbarPathname){
+//         allNavbarList.classList.add("activeNavbarSection")
+//     }
    
-}
-)
+// }
+// )
 
+if(window.innerWidth <= 720){
+  let subTitle = document.querySelector(".subtitle").innerHTML = `<span class="subtitle"
+  >Frontend Marvels: A Glimpse into My Development World</span
+>`
+}
